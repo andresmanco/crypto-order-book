ba2049c5e7a05dc29e223621562b766fe4cb95d7
Andrés Manco <andresmanco@gmail.com>
2026-03-09 09:35:24 -0400

    fix:\ tailwind dependency conflict

--- craco.config.js
+++ craco.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   style: {
     postcss: {
-      plugins: [require("tailwindcss"), require("autoprefixer")],
+      loaderOptions: (postcssLoaderOptions) => {
+        postcssLoaderOptions.postcssOptions.plugins = [
+          require("tailwindcss")("./tailwind.config.js"), // Manually point to the file
+          require("autoprefixer"),
+        ];
+        return postcssLoaderOptions;
+      },
     },
   },
 };
