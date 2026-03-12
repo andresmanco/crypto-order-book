503116f5294a1acaffe4c3be96ab633bb8656b60
Andres Manco <40046378+andresmanco@users.noreply.github.com>
2026-03-12 14:01:56 -0400

    fix: Ladder view retracting when less than 15 asks or bids

--- src/components/order-book/LadderView.jsx
+++ src/components/order-book/LadderView.jsx
@@ -1,5 +1,6 @@
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "../ui/table";
 import { cn, formatPrice } from "../../lib/utils";
+import { MAX_BOOK_ROWS } from "../../constants";
 
 export function LadderView({ rows, type }) {
   return (
@@ -17,12 +18,21 @@ export function LadderView({ rows, type }) {
           const rowKey = typeof row.price === "number" ? `${type}-${formatPrice(row.price)}` : index;
           const price = typeof row.price === "number" ? formatPrice(row.price) : row.price; //---.--
           return (
-            <TableRow className="border-0" key={rowKey}>
+            <TableRow className="border-0 h-[22px]" key={rowKey}>
               <TableCell>{row.qty}</TableCell>
               <TableCell className={cn("text-right", type === "bid" ? "text-bid" : "text-ask")}>{price}</TableCell>
             </TableRow>
           );
         })}
+        {rows.length < MAX_BOOK_ROWS &&
+          Array.from({ length: MAX_BOOK_ROWS - rows.length }).map((_, i) => {
+            return (
+              <TableRow key={`empty-${i}`} className="border-0 h-[22px]">
+                <TableCell />
+                <TableCell />
+              </TableRow>
+            );
+          })}
       </TableBody>
     </Table>
   );
